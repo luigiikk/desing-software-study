379233a687df90a0febbebf6f9d7aa9043892b17
luigikk <luigimee15@gmail.com>
2025-03-13 19:20:48 -0300

    ✨feat: add method create in question

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,25 @@
 import type { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import type { Optional } from "../../core/types/optional"
 
 interface QuestionProps {
   title: string
   content: string
   slug: Slug
-  authorId: string 
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {  
+  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+    const question = new Question({
+      ...props,
+      createdAt: new Date(),
+    }, id)
+
+    return question
+  }
 }
