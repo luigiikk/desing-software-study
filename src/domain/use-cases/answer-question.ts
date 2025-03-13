25c902818a6c3ffd9fbabfd54fac25ef36553263
luigikk <luigimee15@gmail.com>
2025-03-13 19:21:59 -0300

    ♻️refactor: adjusts answer question use case

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import type { AnswerRepository } from "../../repositories/answer-repository"
 import { Answer } from "../entities/answer"
 
@@ -13,10 +14,10 @@ export class AnswerQuestionUseCase {
     private answersRepository: AnswerRepository,
   ) {}
  async execute({instructorId, questionId, content} : AnswerQuestionUseCaseRequest){
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
     })
 
     await this.answersRepository.create(answer)
