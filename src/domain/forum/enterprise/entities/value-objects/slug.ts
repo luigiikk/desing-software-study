669ae884e5353c7a0822deed4fed78ee9e6937ac
luigikk <luigimee15@gmail.com>
2025-03-14 20:54:33 -0300

    🧪test: create search get question by slug test

--- src/domain/forum/enterprise/entities/value-objects/slug.ts
+++ src/domain/forum/enterprise/entities/value-objects/slug.ts
@@ -1,9 +1,13 @@
 export class Slug {
   public value: string
 
-  constructor(value: string){
+  private constructor(value: string){
     this.value = value
   }
+  
+  static create(slug: string){
+    return new Slug(slug)
+  } 
 
   static createFromText(text: string){
     const slugText = text
