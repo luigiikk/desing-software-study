72fbf2c5d7f233b88b2c828f3b071ec4f798e734
luigikk <luigimee15@gmail.com>
2025-03-15 18:51:29 -0300

    ✨feat: create make answer factories

---
+++ test/factories/make-answer.ts
@@ -0,0 +1,27 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { faker } from '@faker-js/faker'
+import {
+  Answer,
+  type AnswerProps,
+} from '@/domain/forum/enterprise/entities/answer'
+
+export function makeAnswer(
+  override: Partial<AnswerProps> = {},
+
+  id?: UniqueEntityID
+) {
+  const answer = Answer.create(
+    {
+      questionId: new UniqueEntityID(),
+
+      authorId: new UniqueEntityID(),
+
+      content: faker.lorem.text(),
+
+      ...override,
+    },
+    id
+  )
+
+  return answer
+}
