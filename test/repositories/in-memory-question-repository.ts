231028bba92f3b749bcf7423e6e00bd1fbfd580d
luigikk <luigimee15@gmail.com>
2025-03-17 15:32:56 -0300

    ✨ feat: fetch recent question use case

--- test/repositories/in-memory-question-repository.ts
+++ test/repositories/in-memory-question-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 import type { QuestionRepository } from '@/domain/forum/application/repositories/questions-repository'
 import type { Question } from '@/domain/forum/enterprise/entities/question'
 
@@ -20,6 +21,13 @@ export class InMemoryQuestionRepository implements QuestionRepository {
       return null
     }
     return question
+  }
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async create(question: Question) {
