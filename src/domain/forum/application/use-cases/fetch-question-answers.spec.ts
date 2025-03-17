814a26c04d59c8af851b794d2ea996d2fd22e573
luigikk <luigimee15@gmail.com>
2025-03-17 16:01:00 -0300

    🧪test: fetch  question answer test

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryAnswerRepository } from 'test/repositories/in-memory-answer-repository'
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers'
+import { makeAnswer } from 'test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswerRepository: InMemoryAnswerRepository
+let sut: FetchQuestionAnswersUseCase
+describe('Fetch Recent Question', () => {
+  beforeEach(() => {
+    inMemoryAnswerRepository = new InMemoryAnswerRepository()
+    sut = new FetchQuestionAnswersUseCase(inMemoryAnswerRepository)
+  })
+
+  it('should be able to fetch recent questions', async () => {
+    await inMemoryAnswerRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+    await inMemoryAnswerRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+    await inMemoryAnswerRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(answers).toHaveLength(3)
+  })
+})
