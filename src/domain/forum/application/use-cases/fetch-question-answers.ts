d9f67fe2150970cfc57ffb72fc94b911ee19c6c6
luigikk <luigimee15@gmail.com>
2025-03-17 16:00:46 -0300

    ✨ feat: fetch question answer use case

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,28 @@
+import type { AnswerRepository } from '../repositories/answer-repository'
+import type { Answer } from '../../enterprise/entities/answer'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answerRepository: AnswerRepository) {}
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answerRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    )
+
+    return {
+      answers,
+    }
+  }
+}
--- test/repositories/in-memory-answer-repository.ts
+++ test/repositories/in-memory-answer-repository.ts
@@ -1,7 +1,10 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 import type { AnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
 import type { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswerRepository implements AnswerRepository {
+  public items: Answer[] = []
+
   async findById(id: string) {
     const answer = this.items.find(item => item.id.toString() === id)
 
@@ -12,7 +15,13 @@ export class InMemoryAnswerRepository implements AnswerRepository {
     return answer
   }
 
-  public items: Answer[] = []
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter(item => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answers
+  }
 
   async create(answer: Answer) {
     this.items.push(answer)
@@ -24,7 +33,7 @@ export class InMemoryAnswerRepository implements AnswerRepository {
     this.items.splice(itemIndex, 1)
   }
 
-  async save(answer: Answer){
+  async save(answer: Answer) {
     const itemIndex = this.items.findIndex(item => item.id === answer.id)
 
     this.items[itemIndex] = answer
