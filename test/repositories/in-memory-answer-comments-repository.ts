ada8f3003272d2e35ed817ffe859760cb0f55ae5
luigikk <luigimee15@gmail.com>
2025-03-18 20:22:04 -0300

    ✨feat: add find many answer comment by id method

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import type { PaginationParams } from '@/core/repositories/pagination-params'
 import type { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import type { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -14,6 +15,13 @@ export class InMemoryAnswerCommentsRepository
     }
 
     return answerComment
+  }
+  async findManyByAnswerId(answerId: string, params: PaginationParams) {
+    const answerComments = this.items
+      .filter(item => item.answerId.toString() === answerId)
+      .slice((params.page - 1) * 20, params.page * 20)
+
+    return answerComments
   }
 
   async create(answerComment: AnswerComment) {
