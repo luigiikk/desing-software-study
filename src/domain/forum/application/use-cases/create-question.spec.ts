f38e85dcf28cb848d739244d05aa507472faf633
luigikk <luigimee15@gmail.com>
2025-03-24 19:30:36 -0300

    🧪test: add attachments in create question test

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-question-repository'
 import { CreateQuestionUseCase } from './create-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository
 let sut: CreateQuestionUseCase
@@ -14,8 +15,14 @@ describe('Create Question use case', () => {
       authorId: '1',
       title: 'Titulo',
       content: 'Nova Resposta',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
