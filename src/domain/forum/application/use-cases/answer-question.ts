306ccae4217009d8e4df194f03c48e6f2e5b8ee2
luigikk <luigimee15@gmail.com>
2025-03-28 22:14:34 -0300

    ✨feat: add create attachments in answer

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -2,10 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Answer } from '../../enterprise/entities/answer'
 import type { AnswerRepository } from '../repositories/answer-repository'
 import { right, type Either } from '@/core/either'
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment'
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list'
 
 interface AnswerQuestionUseCaseRequest {
   instructorId: string
   questionId: string
+  attachmentsIds: string[]
   content: string
 }
 
@@ -16,12 +19,21 @@ export class AnswerQuestionUseCase {
     instructorId,
     questionId,
     content,
+    attachmentsIds,
   }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
     const answer = Answer.create({
       content,
       authorId: new UniqueEntityID(instructorId),
       questionId: new UniqueEntityID(questionId),
     })
+    const answerAttachment = attachmentsIds.map(attachmentsIds => {
+      return AnswerAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentsIds),
+        answerId: answer.id,
+      })
+    })
+
+    answer.attachments = new AnswerAttachmentList(answerAttachment)
 
     await this.answersRepository.create(answer)
 
