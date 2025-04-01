e98fffc5850a33af3eee0fc80dc436f31189bf7e
luigikk <luigimee15@gmail.com>
2025-03-31 23:57:16 -0300

    ✨feat: add findbyid and save notification repository

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -1,8 +1,25 @@
-import type { NotificationRepository } from "@/domain/notification/application/repositories/notification-repository"
-import type { Notification } from "@/domain/notification/enterprise/entities/notification"
+import type { NotificationRepository } from '@/domain/notification/application/repositories/notification-repository'
+import type { Notification } from '@/domain/notification/enterprise/entities/notification'
 
 export class InMemoryNotificationRepository implements NotificationRepository {
   public items: Notification[] = []
+
+  async findById(id: string) {
+    const notification = this.items.find(
+      item => item.id.toString() === id
+    )
+
+    if (!notification) {
+      return null
+    }
+    return notification
+  }
+
+  async save(notification: Notification) {
+    const itemIndex = this.items.findIndex(item => item.id === notification.id)
+
+    this.items[itemIndex] = notification
+  }
 
   async create(notification: Notification) {
     this.items.push(notification)
