e4ce916ba632d24823ff9bdf2f8df21581785c70
luigikk <luigimee15@gmail.com>
2025-03-31 23:57:33 -0300

    ✨feat: create read notification use case

---
+++ src/domain/notification/application/use-cases/read-notification.ts
@@ -0,0 +1,39 @@
+import { left, right, type Either } from '@/core/either'
+import type { Notification } from '../../enterprise/entities/notification'
+import type { NotificationRepository } from '../repositories/notification-repository'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
+
+interface ReadNotificationUseCaseRequest {
+  recipientId: string
+  notificationId: string
+}
+
+type ReadNotificationUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  { notification: Notification }
+>
+
+export class ReadNotificationUseCase {
+  constructor(private notificationRepository: NotificationRepository) {}
+  async execute({
+    recipientId,
+    notificationId,
+  }: ReadNotificationUseCaseRequest): Promise<ReadNotificationUseCaseResponse> {
+    const notification =
+      await this.notificationRepository.findById(notificationId)
+
+    if (!notification) {
+      return left(new ResourceNotFoundError())
+    }
+
+    if (recipientId !== notification.recipientId.toString()) {
+      return left(new NotAllowedError())
+    }
+
+    notification.read()
+    await this.notificationRepository.save(notification)
+
+    return right({ notification })
+  }
+}
