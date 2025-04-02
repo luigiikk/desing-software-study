0c2dcce8121aafb1216cf988662287f546bb50a8
luigikk <luigimee15@gmail.com>
2025-04-01 23:08:34 -0300

    ✨feat: create the subscriber answer event

---
+++ src/domain/forum/application/subscribers/on-answer-created.ts
@@ -0,0 +1,18 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import type { EventHandler } from '@/core/events/event-handler'
+import { AnswerCreatedEvent } from '../../enterprise/events/answer-created-event'
+
+export class OnAnswerCreated implements EventHandler {
+  constructor() {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendNewAnswerNotification.bind(this),
+      AnswerCreatedEvent.name
+    )
+  }
+
+  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {}
+}
